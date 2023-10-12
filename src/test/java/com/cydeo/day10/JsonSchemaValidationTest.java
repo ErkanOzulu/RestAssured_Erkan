ec485fe3673872f6068c825780699e3e66bfd842
Erkan <ErkanOzulu@users.noreply.github.com>
2023-10-12 11:18:52 +0300

    SchemaValidation

--- src/test/java/com/cydeo/day10/JsonSchemaValidationTest.java
+++ src/test/java/com/cydeo/day10/JsonSchemaValidationTest.java
@@ -52,7 +52,7 @@ public class JsonSchemaValidationTest  extends SpartanAuthTestBase {
     //homework
     //put your post json schema under day10
     //post one spartan using dynamic input(name,gender,phone)
-    //verify your post response matching with json schema,,
+    //verify your post response matching with json schema,
 
 
 
